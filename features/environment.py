e0b5f513fb0757d406d0c5eb4ecbad28fdfcf144
Boucheffa <boucheffa_badis@outlook.com>
2026-01-19 11:07:46 +0100

    test4

--- features/environment.py
+++ features/environment.py
@@ -1,16 +1,25 @@
 # environment.py
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from manage_data.read_data import read_data_from_json
+from selenium.webdriver.chrome.options import Options
+
 
 def before_all(context):
     #Je pense faut ajouter les options pour le webdriver (mode headless, no sandbox ...etc)
 
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless=new")  # Mode headless moderne
+    chrome_options.add_argument("--no-sandbox")  # Indispensable en CI
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
     # Installer et configurer ChromeDriver
     service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Chrome(service=service, options=chrome_options)
     context.driver.maximize_window()
     context.driver.implicitly_wait(15)  # ← AJOUTE ÇA SEULEMENT
 
